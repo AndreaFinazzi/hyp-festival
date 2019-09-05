3a27f279170fa82d4231053c2b1de47a6875be5d
Andrea Finazzi <af.andreafinazzi@gmail.com>
2019-09-05 19:07:50 +0200

    meta fix

--- frontend/public/assets/js/extras/index.js
+++ frontend/public/assets/js/extras/index.js
@@ -57,15 +57,6 @@ const initSlider = function () {
             },
             items: 3
         });
-
-
-        $(".countdown")
-            .countdown("2019/12/01", function (event) {
-                $(this).html(
-                    event.strftime('<div>%w <span>Weeks</span></div>  <div>%D <span>Days</span></div>  <div>%H<span>Hours</span></div> <div>%M<span>Minutes</span></div> <div>%S<span>Seconds</span></div>')
-                );
-            });
-
     });
 }
 
